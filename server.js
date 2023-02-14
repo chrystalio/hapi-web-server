e454dfc6b9e95f66255d78f3177d512a41849ef7
chrystalio <chrystalio.k@outlook.com>
2023-02-14 22:25:48 +0700

    Set server.route method

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
 
 const init = async () => {
     const server = Hapi.server({
@@ -6,24 +8,10 @@ const init = async () => {
         host: 'localhost',
     });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Hello World!';
-        }
-    })
-
-    server.route({
-        method: 'GET',
-        path: '/about',
-        handler: (request, h) => {
-            return 'About Page';
-        }
-    })
+    server.route(routes);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
-}
+};
 
 init();
